c114a6651f02b7f8ee778913659099ddd8cba8ea
Mariya <s1227664@sms.ed.ac.uk>
2020-10-02 14:37:09 +0000

    CRUD operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,53 @@
 class ArticlesController < ApplicationController
+	skip_before_action :verify_authenticity_token
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
 
 	def show
-		# to retrieve an article 
-		#article = Article.find(params[:id]) #pass the id from the db to the application, using the params hash
-		#but this variable won't show up in the View (it is only available in the method here), we need to instead use an instance variable
-		@article = Article.find(params[:id])
 	end 
 	
+	def index
+		@articles = Article.all
+	end 
+	
+	def new
+		@article = Article.new
+	end
+	
+	def edit
+
+	end 
+	
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:notice] = "Article was created successfully"
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+	
+	def update
+		if @article.update(article_params)
+			flash[:notice] = "Article was updated successfully"
+			redirect_to @article
+		else
+			render 'edit'
+		end 
+	end
+	
+	def destroy
+		@article.destroy
+		redirect_to articles_path
+	end
+	
+	private
+	
+	def set_article
+		@article = Article.find(params[:id])
+	end
+	
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
 	root 'pages#home'
 	get 'about', to: 'pages#about'
 	
-	#resources :articles #all of the routes for articles that we want
-	resources :articles, only: [:show] #this will keep the route for show only
+	resources :articles
 end

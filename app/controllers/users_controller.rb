e0022acb600075342cb4601d7b8cb2b024277219
Mariya <s1227664@sms.ed.ac.uk>
2021-01-06 16:36:27 +0000

    Add user sessions and helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-	 skip_before_action :verify_authenticity_token
+	skip_before_action :verify_authenticity_token
+	
+	before_action :set_user, only: [:show, :edit, :update]
+	
+	def index
+		@users = User.paginate(page: params[:page], per_page: 2)
+	end 
 	
 	def new
 		@user = User.new
@@ -8,6 +14,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, your registration is complete"
 			redirect_to articles_path
 		else
@@ -15,10 +22,35 @@ class UsersController < ApplicationController
 		end 
 	end
 	
+	def edit
+	end 
+	
+	def update
+		if @user.update(user_params)
+			flash[:notice] = "Your profile was updated"
+			redirect_to @user
+		else 
+			render 'edit'
+		end 
+	end
+	
+	def show
+
+		@articles = @user.articles.paginate(page: params[:page], per_page: 5)
+	end 
+	
 	private
+	
+	# def set_user
+	# 	@user = User.find(params[:id])
+	# end 
 	
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
+	end
+	
+	def set_user
+		@user = User.find(params[:id])
 	end 
 	
 end 

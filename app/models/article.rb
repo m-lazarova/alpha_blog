c73be3fcf66a7267e958f470a82a28d240c0c5e4
Mariya <s1227664@sms.ed.ac.uk>
2021-01-22 17:41:49 +0000

    Associate categories with articles, display categories on articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ApplicationRecord
 	belongs_to :user
 	
+	has_many :article_categories
+	has_many :categories, through: :article_categories
+	
 	validates :title, presence: true, length: {minimum: 5, maximum: 100} 
 	#make sure that the title is present in order for an article to be saved, minimum length is 5 chars 
 	validates :description, presence: true, length: {minimum: 10, maximum: 300}
